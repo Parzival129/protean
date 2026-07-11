f3ba3083fce59a08ead942cba2c6d0a83c007e48
Parzival129 <beacheshiro@gmail.com>
2026-07-10 23:35:53 -0400

    finished first version of SPI byte engine

--- src/spi.v
+++ src/spi.v
@@ -4,8 +4,8 @@ module spi #(
     input wire clk,
     input wire start,
     input wire [7:0] tx,
-    output wire done,
-    output wire [7:0] rx,
+    output reg done,
+    output reg [7:0] rx,
     output reg sclk = 1'b0,
     output wire mosi,
     output reg led = 1'b0,
@@ -14,11 +14,22 @@ module spi #(
 
 reg tick;
 reg [23:0] cnt = 10'd0;
+reg busy = 1'b0; // 0 idle, 1 mid-transfer
+reg [5:0] edge_cnt;
+reg [7:0] tx_shift;
+
+assign mosi = tx_shift[7];
 
 always @(posedge clk) begin // clock divider
     
     tick <= 1'b0;
+    done <= 0;
 
+    if (!busy && start) begin
+        busy <= 1'd1; // transfer begin
+        tx_shift <= tx;
+
+    end
     if (cnt == N-1) begin
         tick <= 1'b1;
         cnt <= 0;
@@ -26,10 +37,26 @@ always @(posedge clk) begin // clock divider
     else begin
         cnt <= cnt + 1'd1;
     end
-    if (tick) begin
+    if (busy && tick) begin // once transfer begins start edge counting
+        
         led <= ~led; // just for testing
-        sclk <= ~sclk;
+        sclk <= ~sclk; // takes effect at end of always block (non blocking)
+        
+        if (sclk == 1) begin // shifts out the tx to mosi every falling edge by most significant bit at a time.
+            tx_shift <= {tx_shift[6:0], 1'b0}; // sclk is checked to be 1 because sclk toggle hasn't taken effect yet.
+        end
+        if (sclk == 0) begin // shifts data to rx from miso, recieving it bit by bit
+            rx <= {rx[6:0], miso};
+        end
+        
+        edge_cnt <= edge_cnt + 1'd1;
     end
+    if (edge_cnt == 16) begin // stop when weve toggled the clock 16 times (8 bits)
+        edge_cnt <= 0;
+        busy <= 0;
+        done <= 1;
+    end
+    
 end
 
 endmodule
